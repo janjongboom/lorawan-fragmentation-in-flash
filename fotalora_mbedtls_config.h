608d520102e1ccfe6c5639a25738be2e838d5002
Jan Jongboom <janjongboom@gmail.com>
2018-01-23 11:00:05 +0200

    Time to remove stuff

--- fotalora_mbedtls_config.h
+++ fotalora_mbedtls_config.h
@@ -1,59 +1,95 @@
-/**
- *  Minimal configuration of features that do not require an entropy source
- *
- *  Copyright (C) 2016, ARM Limited, All Rights Reserved
- *  SPDX-License-Identifier: Apache-2.0
- *
- *  Licensed under the Apache License, Version 2.0 (the "License"); you may
- *  not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *  http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- *  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- *
- *  This file is part of mbed TLS (https://tls.mbed.org)
- */
-/*
- * Minimal configuration of features that do not require an entropy source
- * Distinguishing reatures:
- * - no entropy module
- * - no TLS protocol implementation available due to absence of an entropy
- *   source
- *
- * See README.txt for usage instructions.
- */
+#ifndef FOTALORA_MBEDTLS_CONFIG_H
 
-#ifndef MBEDTLS_CONFIG_H
-#define MBEDTLS_CONFIG_H
+#include "platform/inc/platform_mbed.h"
 
-/* System support */
+#define FOTALORA_MBEDTLS_CONFIG_H
+
 #define MBEDTLS_HAVE_ASM
 #define MBEDTLS_HAVE_TIME
+#define MBEDTLS_CIPHER_MODE_CBC
+#define MBEDTLS_CIPHER_PADDING_PKCS7
 
-/* mbed TLS feature support */
+#define MBEDTLS_REMOVE_ARC4_CIPHERSUITES
+
 #define MBEDTLS_ECP_DP_SECP256R1_ENABLED
+#define MBEDTLS_ECP_DP_SECP384R1_ENABLED
+#define MBEDTLS_ECP_DP_CURVE25519_ENABLED
+
+#define MBEDTLS_ECP_NIST_OPTIM
+
 #define MBEDTLS_ECDSA_DETERMINISTIC
 
-/* mbed TLS modules */
+#define MBEDTLS_ERROR_STRERROR_DUMMY
+
+#define MBEDTLS_NO_PLATFORM_ENTROPY
+
+#define MBEDTLS_VERSION_FEATURES
+
+#define MBEDTLS_X509_CHECK_KEY_USAGE
+
+#define MBEDTLS_X509_CHECK_EXTENDED_KEY_USAGE
+
+#define MBEDTLS_AES_C
+
 #define MBEDTLS_ASN1_PARSE_C
+
 #define MBEDTLS_ASN1_WRITE_C
+
+#define MBEDTLS_BASE64_C
+
 #define MBEDTLS_BIGNUM_C
+
+#define MBEDTLS_CCM_C
+
+#define MBEDTLS_CERTS_C
+
+#define MBEDTLS_CIPHER_C
+
+#define MBEDTLS_CTR_DRBG_C
+
+#define MBEDTLS_DEBUG_C
+
+#define MBEDTLS_ECDH_C
+
 #define MBEDTLS_ECDSA_C
+
 #define MBEDTLS_ECP_C
+
+#define MBEDTLS_ENTROPY_C
+
 #define MBEDTLS_ERROR_C
+
+#define MBEDTLS_GCM_C
+
 #define MBEDTLS_HMAC_DRBG_C
+
 #define MBEDTLS_MD_C
+
 #define MBEDTLS_OID_C
+
+#define MBEDTLS_PEM_PARSE_C
+
 #define MBEDTLS_PK_C
+
 #define MBEDTLS_PK_PARSE_C
+
+#define MBEDTLS_PK_WRITE_C
+
 #define MBEDTLS_PLATFORM_C
+
 #define MBEDTLS_SHA256_C
+
+#define MBEDTLS_SHA512_C
+
+#define MBEDTLS_VERSION_C
+
+#define MBEDTLS_X509_USE_C
+
+#define MBEDTLS_X509_CRT_PARSE_C
+
+#define MBEDTLS_X509_CRL_PARSE_C
+
 
 #include "check_config.h"
 
-#endif /* MBEDTLS_CONFIG_H */
+#endif /* FOTALORA_MBEDTLS_CONFIG_H */
