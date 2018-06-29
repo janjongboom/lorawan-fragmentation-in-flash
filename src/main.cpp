af658843fadc3383ea0a4e7afdcc4217b46befb1
Jan Jongboom <janjongboom@gmail.com>
2018-06-29 13:46:31 +0800

    Non-delta updates should work

--- src/main.cpp
+++ src/main.cpp
@@ -26,12 +26,13 @@
 #ifdef TARGET_SIMULATOR
 // Initialize a persistent block device with 528 bytes block size, and 256 blocks (mimicks the at45, which also has 528 size blocks)
 #include "SimulatorBlockDevice.h"
-SimulatorBlockDevice bd("lorawan-frag-in-flash", 256 * 528, 528);
+SimulatorBlockDevice bd("lorawan-frag-in-flash", 256 * 528, static_cast<uint64_t>(528));
 #else
 // Flash interface on the L-TEK xDot shield
 #include "AT45BlockDevice.h"
 AT45BlockDevice bd(SPI_MOSI, SPI_MISO, SPI_SCK, SPI_NSS);
 #endif
+
 
 // fwd declaration
 static void fake_send_method(uint8_t, uint8_t*, size_t);
@@ -70,28 +71,29 @@ static void fake_send_method(uint8_t port, uint8_t *data, size_t length) {
         printf("%02x ", data[ix]);
     }
     printf("\n");
+}
 
-    // CRC64 hash validation...
-    if (port == 201 && length == 10 && data[0] == 0x5) {
-        is_complete = true;
+static void lorawan_uc_event_handler(LW_UC_EVENT event) {
+    switch (event) {
+        case LW_UC_EVENT_FRAGSESSION_COMPLETE:
+            is_complete = true;
+            break;
 
-        printf("FragmentationSession is complete\n");
+        case LW_UC_EVENT_FIRMWARE_READY:
+            printf("Firmware is ready - resetting device to flash new firmware...\n");
 
-        // This hash needs to be sent to the network to verify that the packet originated from the network
-        if (compare_buffers((uint8_t*)&FAKE_PACKETS_CRC64_HASH, data + 2, 8)) {
-            printf("CRC64 Hash verification OK (%08llx)\n", FAKE_PACKETS_CRC64_HASH);
-        }
-        else {
-            printf("CRC64 Hash verification NOK, hash was %08llx, expected %08llx\n", ((uint64_t*)data + 2)[0], FAKE_PACKETS_CRC64_HASH);
-        }
-
-        // @todo, inject the OK/NOK packet.
+#ifndef TARGET_SIMULATOR
+            NVIC_SystemReset();
+#endif
+            break;
     }
 }
 
 int main() {
 
     LW_UC_STATUS status;
+
+    uc.setEventCallback(&lorawan_uc_event_handler);
 
     status = uc.handleFragmentationCommand((uint8_t*)FAKE_PACKETS_HEADER, sizeof(FAKE_PACKETS_HEADER));
     if (status != LW_UC_OK) {
@@ -101,10 +103,12 @@ int main() {
 
     // Process the frames in the FAKE_PACKETS array
     for (size_t ix = 0; ix < sizeof(FAKE_PACKETS) / sizeof(FAKE_PACKETS[0]); ix++) {
+        if (is_complete) break;
+
         status = uc.handleFragmentationCommand((uint8_t*)FAKE_PACKETS[ix], sizeof(FAKE_PACKETS[0]));
 
         if (status != LW_UC_OK) {
-            printf("FragmentationSession process_frame failed: %s\n", status);
+            printf("FragmentationSession process_frame failed: %u\n", status);
             return 1;
         }
 
@@ -116,130 +120,6 @@ int main() {
 
         wait_ms(50); // @todo: this is really weird, writing these in quick succession leads to corrupt image... need to investigate.
     }
-
-    // // The data is now in flash. Free the fragSession
-    // delete fragSession;
-
-    // // Calculate the CRC of the data in flash to see if the file was unpacked correctly
-    // uint64_t crc_res;
-    // // To calculate the CRC on desktop see 'calculate-crc64/main.cpp'
-    // {
-    //     uint8_t crc_buffer[128];
-
-    //     FragmentationCrc64 crc64(&fbd, crc_buffer, sizeof(crc_buffer));
-    //     crc_res = crc64.calculate(opts.FlashOffset, (opts.NumberOfFragments * opts.FragmentSize) - opts.Padding);
-
-    //     // This hash needs to be sent to the network to verify that the packet originated from the network
-    //     if (FAKE_PACKETS_CRC64_HASH == crc_res) {
-    //         printf("CRC64 Hash verification OK (%08llx)\n", crc_res);
-    //     }
-    //     else {
-    //         printf("CRC64 Hash verification NOK, hash was %08llx, expected %08llx\n", crc_res, FAKE_PACKETS_CRC64_HASH);
-    //         return 1;
-    //     }
-    // }
-
-    // wait_ms(1);
-
-    // // the signature is the last FOTA_SIGNATURE_LENGTH bytes of the package
-    // size_t signatureOffset = opts.FlashOffset + ((opts.NumberOfFragments * opts.FragmentSize) - opts.Padding) - FOTA_SIGNATURE_LENGTH;
-
-    // // Read out the header of the package...
-    // UpdateSignature_t* header = new UpdateSignature_t();
-    // fbd.read(header, signatureOffset, FOTA_SIGNATURE_LENGTH);
-
-    // if (!compare_buffers(header->manufacturer_uuid, UPDATE_CERT_MANUFACTURER_UUID, 16)) {
-    //     printf("Manufacturer UUID does not match\n");
-    //     return 1;
-    // }
-
-    // if (!compare_buffers(header->device_class_uuid, UPDATE_CERT_DEVICE_CLASS_UUID, 16)) {
-    //     printf("Device Class UUID does not match\n");
-    //     return 1;
-    // }
-
-    // printf("Manufacturer and Device Class UUID match\n");
-
-    // wait_ms(1);
-
-    // // Calculate the SHA256 hash of the file, and then verify whether the signature was signed with a trusted private key
-    // unsigned char sha_out_buffer[32];
-    // {
-    //     uint8_t sha_buffer[128];
-
-    //     // SHA256 requires a large buffer, alloc on heap instead of stack
-    //     FragmentationSha256* sha256 = new FragmentationSha256(&fbd, sha_buffer, sizeof(sha_buffer));
-
-    //     // // The last FOTA_SIGNATURE_LENGTH bytes are reserved for the sig, so don't use it for calculating the SHA256 hash
-    //     sha256->calculate(
-    //         opts.FlashOffset,
-    //         (opts.NumberOfFragments * opts.FragmentSize) - opts.Padding - FOTA_SIGNATURE_LENGTH,
-    //         sha_out_buffer);
-
-    //     printf("SHA256 hash is: ");
-    //     for (size_t ix = 0; ix < 32; ix++) {
-    //         printf("%02x", sha_out_buffer[ix]);
-    //     }
-    //     printf("\n");
-
-    //     // now check that the signature is correct...
-    //     {
-    //         printf("ECDSA signature is: ");
-    //         for (size_t ix = 0; ix < header->signature_length; ix++) {
-    //             printf("%02x", header->signature[ix]);
-    //         }
-    //         printf("\n");
-    //         printf("Verifying signature...\n");
-
-    //         // ECDSA requires a large buffer, alloc on heap instead of stack
-    //         FragmentationEcdsaVerify* ecdsa = new FragmentationEcdsaVerify(UPDATE_CERT_PUBKEY, UPDATE_CERT_LENGTH);
-    //         bool valid = ecdsa->verify(sha_out_buffer, header->signature, header->signature_length);
-    //         if (!valid) {
-    //             printf("ECDSA verification of firmware failed\n");
-    //             return 1;
-    //         }
-    //         else {
-    //             printf("ECDSA verification OK\n");
-    //         }
-    //     }
-    // }
-
-    // wait_ms(1);
-
-    // free(header);
-
-    // // Hash is matching, now write the header so the bootloader can flash the update
-    // arm_uc_firmware_details_t details;
-    // details.version = static_cast<uint64_t>(MBED_BUILD_TIMESTAMP) + 10; // should be timestamp that the fw was built, this is to get around this
-    // details.size = (opts.NumberOfFragments * opts.FragmentSize) - opts.Padding - FOTA_SIGNATURE_LENGTH;
-    // memcpy(details.hash, sha_out_buffer, 32); // SHA256 hash of the firmware
-    // memset(details.campaign, 0, ARM_UC_GUID_SIZE); // todo, add campaign info
-    // details.signatureSize = 0; // not sure what this is used for
-
-    // uint8_t *fw_header_buff = (uint8_t*)malloc(ARM_UC_EXTERNAL_HEADER_SIZE_V2);
-    // if (!fw_header_buff) {
-    //     printf("Could not allocate %d bytes for header\n", ARM_UC_EXTERNAL_HEADER_SIZE_V2);
-    //     return 1;
-    // }
-
-    // arm_uc_buffer_t buff = { ARM_UC_EXTERNAL_HEADER_SIZE_V2, ARM_UC_EXTERNAL_HEADER_SIZE_V2, fw_header_buff };
-
-    // arm_uc_error_t err = arm_uc_create_external_header_v2(&details, &buff);
-
-    // if (err.error != ERR_NONE) {
-    //     printf("Failed to create external header (%d)\n", err.error);
-    //     return 1;
-    // }
-
-    // int r = fbd.program(buff.ptr, MBED_CONF_APP_FRAGMENTATION_BOOTLOADER_HEADER_OFFSET, buff.size);
-    // if (r != BD_ERROR_OK) {
-    //     printf("Failed to program firmware header: %d bytes at address 0x%x\n", buff.size, MBED_CONF_APP_FRAGMENTATION_STORAGE_OFFSET);
-    //     return 1;
-    // }
-
-    // printf("Stored the update parameters in flash on 0x%x. Reset the board to apply update.\n", MBED_CONF_APP_FRAGMENTATION_STORAGE_OFFSET);
-
-    // bd.deinit();
 
     wait(osWaitForever);
 }
